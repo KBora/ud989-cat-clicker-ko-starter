9e78116f65d083351edb894de1ec7f9101473d6b
wookkii <wookkii@home>
2016-03-13 09:22:25 +1100

    Bind cat name

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 var ViewModel = function() {
+	// this is the model (inside the view currently, but can be moved out later)
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	this.imgAttribution = ko.observable('');
 
+
+	this.incremementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	}
+	
 }
 
 ko.applyBindings(new ViewModel);
