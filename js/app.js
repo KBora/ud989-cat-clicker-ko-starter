3152a9656c121e7f994a1d150eb3c0526ba5dcc2
wookkii <wookkii@home>
2016-03-14 23:14:38 +1100

    Create multiple cats using initialCats data object

--- js/app.js
+++ js/app.js
@@ -1,3 +1,53 @@
+var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+            nickNames: [
+							{ nickname: 'Tabitha' },
+							{ nickname: 'Kitty'}, 
+							{ nickname: 'Tiara'}
+						]
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+						nickNames: [
+							{ nickname: 'Tigger' }
+						]
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nickNames: [
+							{ nickname: 'Poo' }
+						]
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            nickNames: [
+							{ nickname: 'Greybeard' }
+						]
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nickNames: [
+							{ nickname: 'Valery' }
+						]
+        }
+    ];
+
 var Cat = function(data) {
 
 	this.clickCount = ko.observable(data.clickCount);
@@ -32,17 +82,14 @@ var ViewModel = function() {
 
 	var self = this;
 
-	this.currentCat = ko.observable( new Cat({
-		clickCount: 0,
-		name: 'Tabby',
-		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-		imgAttribution: 'http://google.com',
-		nickNames: [
-			{ nickname: 'Tabitha' },
-			{ nickname: 'Kitty'}, 
-			{ nickname: 'Tiger'}
-		]
-	}) );
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem));
+	});
+
+
+	this.currentCat = ko.observable( this.catList()[0]) ;
 
 	this.incrementCounter = function() {
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
